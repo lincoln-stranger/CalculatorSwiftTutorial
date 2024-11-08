156b43bcf40ee3242acbf9292be59dd48f87161b
Lincoln Stranger <las535@cornell.edu>
2024-11-08 13:09:38 -0500

    Initial Commit

---
+++ CalculatorSwiftTutorial/CalculatorSwiftTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftTutorialApp.swift
+//  CalculatorSwiftTutorial
+//
+//  Created by Lincoln Stranger on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorSwiftTutorialTests/CalculatorSwiftTutorialTests.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorSwiftTutorialTests.swift
+//  CalculatorSwiftTutorialTests
+//
+//  Created by Lincoln Stranger on 11/8/24.
+//
+
+import Testing
+
+struct CalculatorSwiftTutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
